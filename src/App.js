302d2b67e940d9d6fa2864e7872f2417ea5f524c
Bindhuu <143107119+Bindhhuu@users.noreply.github.com>
2025-07-01 18:21:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
+import Experience from './components/Experience';
 import './components/App.scss';
 
 function App() {
